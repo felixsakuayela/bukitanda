36e0fbf9100cebc40c9c84daf4530e604a2077a7
Felix Angelico Sakuayela <f.sakuayela@gmail.com>
2023-01-04 22:14:22 -0300

    update views

--- greatkart/views.py
+++ greatkart/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from store.models import Product, ReviewRating
+from django.utils import translation
+from django.utils.translation import gettext as _
 
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
